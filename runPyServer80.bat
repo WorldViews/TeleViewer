b9971b7ad666ebaefab8dfbf4fc2d31e1eda722a
donkimber <donkimber@gmail.com>
2021-03-11 17:53:08 -0800

    tweaked script to give correct mimetype for .js files

---
+++ runPyServer80.bat
@@ -0,0 +1,6 @@
+REM This script assumes python 3
+REM
+REM This doesn't give correct mimetypes for JavaScript
+REM python -m http.server --cgi 80
+REM so instead we use this:
+python server80.py
---
+++ server80.py
@@ -0,0 +1,26 @@
+#test on python 3.4 ,python of lower version  has different module organization.
+import http.server
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import socketserver
+
+PORT = 80
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+Handler.extensions_map={
+        '.manifest': 'text/cache-manifest',
+	'.html': 'text/html',
+        '.png': 'image/png',
+	'.jpg': 'image/jpg',
+	'.svg':	'image/svg+xml',
+	'.css':	'text/css',
+	'.js':	'application/x-javascript',
+	'.mjs': 'text/javascript',
+	'': 'application/octet-stream', # Default
+    }
+print("Mime types", Handler.extensions_map)
+
+httpd = socketserver.TCPServer(("", PORT), Handler)
+
+print("serving at port", PORT)
+httpd.serve_forever()
